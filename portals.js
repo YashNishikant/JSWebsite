d3f69eff84a8a2b803b860aa61cded6cba0f8910
Yash Nishikant <yash0312@hotmail.com>
2023-02-25 14:51:02 -0500

    margins

--- portals.js
+++ portals.js
@@ -1,33 +1,36 @@
 function addOnClick(){
 
     var ul = document.getElementById("list")
-    var urltext = document.getElementById("urltext");
-    var nametext = document.getElementById("nametext");
-    var li = document.createElement("li");
+    var urltext = document.getElementById("urltext")
+    var nametext = document.getElementById("nametext")
+    var li = document.createElement("li")
 
-    const str = urltext.value.replace(/\s+/g, '');
+    const str = urltext.value.replace(/\s+/g, '')
+
     if(str!=""){
-        var link = document.createElement('a');
+        var link = document.createElement('a')
         link.target="_blank"
-        var linktext = document.createTextNode(nametext.value);
+        var linktext = document.createTextNode(nametext.value)
         link.title=nametext.value
         link.href=urltext.value
         link.appendChild(linktext)
 
-        li.setAttribute('id', nametext.value);
+        li.setAttribute('id', nametext.value)
         li.appendChild(link)
         
-        var span = document.createElement('span');
-        var img  = document.createElement("img");
-        img.src="Remove.png";
+        var span = document.createElement('span')
+        var img  = document.createElement("img")
+        img.src="Remove.png"
         img.className="removeIcon"
         span.innerHTML=" "
-        li.appendChild(span);
+        li.appendChild(span)
         span.appendChild(img)
         ul.appendChild(li)
+        urltext.value=""
+        nametext.value=""
     }
 
-    var removebuttons = document.querySelectorAll('span');
+    var removebuttons = document.querySelectorAll('span')
     for(let i = 0; i < removebuttons.length; i++){
         removebuttons[i].addEventListener('click', ()=>{
             removebuttons[i].parentElement.remove()
